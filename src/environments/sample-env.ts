088d963862c06cf5111c4d245d2b6271f4b8a83a
baberarjumand <baber.arjumand@gmail.com>
2022-10-13 14:50:04 +0500

    added sample env file

---
+++ src/environments/sample-env.ts
@@ -0,0 +1,7 @@
+// create two files: 'environment.ts' and 'environment.prod.ts'
+// define your own environment variables and
+// copy the following content into those files
+
+export const environment = {
+  production: true,
+};
